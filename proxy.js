2b2d305e5ed6b144df3bce67aaa793f67b1ca02e
hrishirajs <hrs150203@gmail.com>
2026-05-05 22:24:20 +0530

    change proxy file

--- proxy.js
+++ proxy.js
@@ -16,7 +16,7 @@ export default clerkMiddleware(async (auth, req) => {
 });
 
 export const config = {
-  runtime: "nodejs",
+  // runtime: "nodejs"  ← delete this line
   matcher: [
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     "/(api|trpc)(.*)",
